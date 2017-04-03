804c4ec4815a9269d51e355eef3e369551c7235f
Jamie Quinn <prb16236@archie-w.archie.cluster.local>
2017-04-03 19:33:23 +0100

    Build script now dumps some info about what options the binary was built with

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
+build_state_file='bin/build_state'
 machine='office'
 n_grid_points='false'
 braginskii='false'
 switching='false'
 output='true'
-defines=''
 mode=''
-compiler=''
-mpif90='mpif90'
 
 while getopts "bsdom:p:" flag; do
   case "${flag}" in
@@ -26,11 +24,10 @@ while getopts "bsdom:p:" flag; do
   esac
 done
 
+defines=''
 if [ "$braginskii" == 'true' ]; then
   defines+=' -DBRAGINSKII_VISCOSITY'
-fi
-
-if [ "$switching" == 'true' ]; then
+elif [ "$switching" == 'true' ]; then
   defines+=' -DSWITCHING_VISCOSITY'
 fi
 
@@ -38,10 +35,14 @@ if [ "$output" == 'false' ]; then
   defines+=' -DNO_IO'
 fi
 
+compiler=''
+mpif90='mpif90'
 if [ "$machine" == "euclid" ]; then
   export PATH=/usr/lib64/openmpi/bin:$PATH
   compiler='gfortran'
 elif [ "$machine" == "archie" ]; then
+  module load mpi/intel/impi/5.0.3
+  module load compilers/intel/17.0.1
   compiler='intel'
   mpif90='mpiifort'
 else
@@ -54,3 +55,21 @@ if [ "$n_grid_points" != 'false' ]; then
 fi
 
 make MPIF90=$mpif90 COMPILER=$compiler DEFINE="$defines" MODE="$mode"
+
+echo "" > $build_state_file
+echo "$defines" >> $build_state_file
+echo "$mode" >> $build_state_file
+echo "built for $machine" >> $build_state_file
+echo "GRID POINTS:" >> $build_state_file
+echo "$(grep 'n[xyz]_global' src/control.f90)" >> $build_state_file
+echo "$(grep -o 'dt_snapshots = .*' src/control.f90)" >> $build_state_file
+echo "$(grep -o 'nsteps = .*' src/control.f90)" >> $build_state_file
+echo "$(grep -o 't_end = .*' src/control.f90)" >> $build_state_file
+echo "$(grep -o 'data_dir = .*' src/control.f90)" >> $build_state_file
+if grep -q 'dump_mask(20)' src/control.f90 ; then
+  echo "ISO: YES" >> $build_state_file
+fi
+if grep -q 'dump_mask(21)' src/control.f90 ; then
+  echo "ANISO: YES" >> $build_state_file
+fi
+echo "$(grep 'dump_mask([0-9]*\:[0-9]*) =' src/control.f90)" >> $build_state_file
