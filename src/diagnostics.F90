b1c4a8fa0b46ae21e774cfcd161ffc415536aa2a
Jamie Quinn <j.quinn.1@research.gla.ac.uk>
2017-11-18 00:48:09 +0000

    Moved the en_nvars calculation to a more appropriate part

--- src/diagnostics.F90
+++ src/diagnostics.F90
@@ -805,10 +805,6 @@ CONTAINS
     INTEGER :: p1, header_length, varname_idx
     CHARACTER(LEN=c_id_length) :: varnames(en_nvars)
 
-#ifdef OUTPUT_CONTINUOUS_VISC_HEATING
-      en_nvars = en_nvars + 2
-#endif
-
     CALL output_log
 
     INQUIRE(en_unit, POS=p1)
