a6e1fe5d9c097af0ea5e82af85c55a6e4bb22a8d
Jamie Quinn <j.quinn.1@research.gla.ac.uk>
2018-02-02 11:53:33 +0000

    Added ramping down twist option

--- src/boundary.f90
+++ src/boundary.f90
@@ -286,7 +286,17 @@ CONTAINS
       vz(:,ny:ny+2,:) = 0.0_num
     END IF
 
-    twist = 0.025_num*pi*(1.0_num + TANH(2.0_num*(time - 2.0_num)))
+    ! --- simple ramp up twisting ---
+    !twist = 0.025_num*pi*(1.0_num + TANH(2.0_num*(time - 2.0_num)))
+
+    ! --- ramp up twisting then ramp down ---
+    IF (time < 20.0_num) THEN
+      ! ramp up twist
+      twist = 0.025_num*pi*(1.0_num + TANH(2.0_num*(time - 2.0_num)))
+    ELSE
+      ! ramp down twist
+      twist = 0.025_num*pi*(1.0_num - TANH(2.0_num*(time - 20.0_num)))
+    END IF
 
     IF (proc_z_min == MPI_PROC_NULL .AND. zbc_min == BC_OTHER) THEN
       DO iy = -2, ny+2
@@ -346,7 +356,16 @@ CONTAINS
       vz1(:,ny:ny+2,:) = 0.0_num
     END IF
 
-    twist = 0.025_num*pi*(1.0_num + TANH(2.0_num*(time + dt2 - 2.0_num)))
+    !twist = 0.025_num*pi*(1.0_num + TANH(2.0_num*(time + dt2 - 2.0_num)))
+
+    ! --- ramp up twisting then ramp down ---
+    IF (time < 20.0_num) THEN
+      ! ramp up twist
+      twist = 0.025_num*pi*(1.0_num + TANH(2.0_num*(time + dt2 - 2.0_num)))
+    ELSE
+      ! ramp down twist
+      twist = 0.025_num*pi*(1.0_num - TANH(2.0_num*(time + dt2 - 23.0_num)))
+    END IF
 
     IF (proc_z_min == MPI_PROC_NULL .AND. zbc_min == BC_OTHER) THEN
       DO iy = -2, ny+2
